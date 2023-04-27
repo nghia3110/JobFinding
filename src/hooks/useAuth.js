a0c4a31054eb525501460e95659473700e97e2ad
nghia3110 <nghiaxu3110@gmail.com>
2023-04-27 15:03:32 +0700

    Update custom hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,14 +1,15 @@
-import { getUser, getUserInfo } from 'store/auth';
+import { getUser, getUserInfo, getCompany } from 'store/auth';
 import { useSelector } from 'react-redux';
 
 export const useAuth = () => {
     const user = useSelector(getUser);
+    const company = useSelector(getCompany);
     //const userInfo = useSelector(getUserInfo);
-    console.log(user)
-
+    //console.log(user)
     return {
-        isLogged: !!user,
+        isLogged: !!user || !!company,
         user,
+        company
         //userInfo
     };
 };
