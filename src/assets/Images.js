78ccce042e29232c1dd4d2d725a3347ecbd6f01d
nghia3110 <nghiaxu3110@gmail.com>
2023-05-17 23:29:04 +0700

    Add assets

---
+++ src/assets/Images.js
@@ -0,0 +1,6 @@
+const images = {
+    job_finding: require('./images/job_finding.png'),
+    avatar: require('./images/avatar.png')
+};
+
+export { images };
---
+++ src/assets/Svg.js
@@ -0,0 +1,9 @@
+export { default as ArrowLeft } from './svg/arrow-left.svg';
+export { default as Close } from './svg/close.svg';
+export { default as Search } from './svg/search.svg';
+export { default as Location } from './svg/Pin_alt_light.svg';
+export { default as User } from './svg/User.svg';
+export { default as UserFill } from './svg/User_fill.svg';
+export { default as Footer } from './svg/footer.svg';
+export { default as ExpandRight } from './svg/Expand_right.svg';
+export { default as NotFound } from './svg/undraw_notify_re_65on.svg';
